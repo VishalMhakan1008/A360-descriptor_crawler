00cfd920288866e667dd74d1d20e1be6e798f82d
Vishal <Vishal.m@platform3solutions.com>
2023-11-20 15:07:24 +0530

    metadata file reader and processor

--- test/Run.py
+++ test/Run.py
@@ -11,14 +11,22 @@ app = Flask(__name__)
 @app.route('/process_table', methods=['POST'])
 def process_table():
     connection_dto = request.get_json()
-    metaData_file_path = connection_dto['connectionType']
+    metaData_file_path = connection_dto.get('metadataFilePath', None)
     metadata_list = []
+    if metaData_file_path:
+        metadata_processor = MetadataProcessor()
+        metadata_list = metadata_processor.process_metadata(connection_dto)
+
 
     file_processor = FileProcessor()
     flat_file_metaData_list = file_processor.process_file(connection_dto)
 
     filtered_meta = filter_metadata(metadata_list,flat_file_metaData_list)
-    json_list = [jsonpickle.encode(metadata.to_dict()) for metadata in flat_file_metaData_list]
+
+    if filtered_meta:
+        json_list = [jsonpickle.encode(metadata) for metadata in filtered_meta]
+    else:
+        json_list = [jsonpickle.encode(metadata.to_dict()) for metadata in flat_file_metaData_list]
 
     return jsonify(json_list)
 
@@ -27,16 +35,28 @@ def filter_metadata(metadata_list, flat_file_metadata_list):
     filtered_metadata = []
 
     for metadata in metadata_list:
+        matched_data = None
         for flat_file_metadata in flat_file_metadata_list:
             if (
-                metadata.schema_name == flat_file_metadata.schema_name
-                and metadata.table_name == flat_file_metadata.table_name
-                and all(
-                    col.column_name in [flat_col.column_name for flat_col in flat_file_metadata.columns]
-                    for col in metadata.columns
-                )
+                    metadata.schema_name == flat_file_metadata.schema_name
+                    and metadata.table_name == flat_file_metadata.table_name
             ):
-                filtered_metadata.append(metadata)
+                # Filter columns based on metadata column names
+                matched_columns = [
+                    flat_file_metadata.columns[column_name]
+                    for column_name in [col.column_name for col in metadata.columns]
+                    if column_name in flat_file_metadata.columns
+                ]
+
+                if matched_columns:
+                    matched_data = {
+                        "schema_name": flat_file_metadata.schema_name,
+                        "table_name": flat_file_metadata.table_name,
+                        "columns": matched_columns
+                    }
+
+        if matched_data:
+                filtered_metadata.append(matched_data)
 
     return filtered_metadata
 
