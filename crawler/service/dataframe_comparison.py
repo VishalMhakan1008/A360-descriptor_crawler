133dc9e7264c90ae8c132e5fc463946ef477de7c
vijayaraja <vijayaraja.s@platform3solutions.com>
2023-12-07 19:52:56 +0530

    save-response

--- crawler/service/dataframe_comparison.py
+++ crawler/service/dataframe_comparison.py
@@ -121,5 +121,7 @@ def process_chunk(df1_chunk, df2_chunk, first_df, num_partitions1, num_partition
 def get_combination_result(process_id):
     try:
         value = final_result[process_id]
+        return value
     except KeyError:
-        ("Key does not exist in the dictionary.")
+        raise KeyError("invalid key")
+
